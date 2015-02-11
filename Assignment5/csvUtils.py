337b8d5b34f27a13468c12055b9ab01c22f9b2f0
Lara Kattan <larakattan@Molly-Blooms-MacBook.local>
2015-02-11 15:50:57 -0600

    add cvsUtils

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -7,7 +7,15 @@ def writeArtistsTable(artist_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY
     """
-    pass 
+    artists_file = open('artists.csv','w')
+
+    artists_file.write(u'ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY\n')
+
+    for artist in range(len(artist_info_list)):
+        artists_file.write(artist_info_list[artist]['id']+","+artist_info_list[artist]['name']+","+str(artist_info_list[artist]['followers'])+","+str(artist_info_list[artist]['popularity']))
+        artists_file.write('\n')
+
+    artists_file.close() 
       
 def writeAlbumsTable(album_info_list):
     """
@@ -18,4 +26,13 @@ def writeAlbumsTable(album_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
-    pass
+    
+    albums_file = open('albums.csv','w')
+
+    albums_file.write(u'ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY\n')
+
+    for album in range(len(album_info_list)):
+        albums_file.write(album_info_list[album]['artist_id']+","+album_info_list[album]['album_id']+","+album_info_list[album]['name']+","+str(album_info_list[album]['year'])+","+str(album_info_list[album]['popularity']))
+        albums_file.write('\n')
+
+    albums_file.close() 
