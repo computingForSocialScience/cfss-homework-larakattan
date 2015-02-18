6ea855ec27df0fa42d9ba4252bba7a0820927a38
Lara Kattan <larakattan@Molly-Blooms-MacBook.local>
2015-02-18 00:28:51 -0600

    in progress: analyzeNetworks.py

---
+++ Assignment6/analyzeNetworks.py
@@ -0,0 +1,37 @@
+import networkx as nx 
+import numpy as np 
+import pandas as pd 
+
+def readEdgeList(filename):
+
+	df = pd.read_csv(filename) # create a dataframe from the passed in CSV file 
+
+	if (len(df.columns)) != 2: # check that the filename includes only two columns; return error if != 2
+		print ("Warning, the CSV passed to readEdgeList() does not have exactly two columns. Extra columns have been dropped.")
+		df = df.drop(df.columns[2:len(df.columns)],axis=1) # drop columns after the first two 
+		#  need axis = 1 to know to drop columns, not rows 
+		# remember that dataframes are indexed from 0 
+
+	return (df)
+
+def degree(edgeList, in_or_out):
+
+	if in_or_out == "in":
+		return edgeList.ix[:,1].value_counts()
+	elif in_or_out == "out":
+		return edgeList.ix[:,0].value_counts()
+	else:
+		print ("Oops, degrees() needs the second argument to be either 'in' or 'out'.")
+
+	return
+
+def combineEdgelists(edgeList1, edgeList2):
+	pass
+
+def pandasToNetworkX(edgeList):
+
+	pass
+
+def randomCentralNode(inputDiGraph):
+
+	pass
