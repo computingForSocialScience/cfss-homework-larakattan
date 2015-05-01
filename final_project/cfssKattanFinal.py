e80bc46f77be1c5f42d209f1e906e0a3243d6570
Lara Kattan <larakattan@gmail.com>
2015-04-30 21:27:13 -0500

    Update cfssKattanFinal.py

--- final_project/cfssKattanFinal.py
+++ final_project/cfssKattanFinal.py
@@ -15,7 +15,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def make_index_resp():
-    # this function just renders templates/index.html when
-    # someone goes to http://127.0.0.1:5000/
     return(render_template('index.html'))
     
